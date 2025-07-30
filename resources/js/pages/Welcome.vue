6afdc39e7893f63712a2c0797f631ed4372d4104
akosialbean <alvincastor30@gmail.com>
2025-07-30 13:40:11 +0800

    Refactor code structure for improved readability and maintainability

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -9,6 +9,10 @@
     .best-deals {
         font-family: 'Bangers', cursive;
     }
+
+    .invert-color {
+        filter: invert(1);
+    }
 </style>
 
 <template>
@@ -17,7 +21,7 @@
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" /> -->
     </Head>
     <TopNavbar />
-    <div class="w-full py-8 mt-4">
+    <div class="w-full">
         <img src="/images/crv.png" alt="Honda Cars Carmona" class="w-full h-auto object-cover mt-7" />
         <div>
             <a href="#" class="bg-red-700 text-white mx-auto px-4 py-2 rounded-md hover:bg-red-800 transition-colors duration-300 text-center mt-10 font-semibold block max-w-xs mx-auto">
@@ -69,8 +73,25 @@
         </div>
 
         <!-- FOOTER -->
-        <div class="bg-gray-900 mt-10 px-4 py-4 bottom-0 text-white text-center">
-            test
+        <div class="bg-gray-900 px-4 py-4 bottom-0 text-white text-center mt-10">
+            <div class="container mx-auto py-12">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
+                    <div>
+                        <img src="/images/logo.png" alt="Honda Cars Carmona Logo" class="w-24 h-auto mb-4 mx-auto invert-color">
+                        <p>Honda Cars Camona SLEX toll </p>
+                    </div>
+
+                    <div>
+                        <h3 class="text-lg font-semibold mb-2">Contact Us</h3>
+                        <p class="text-gray-400">Email: </p>
+                    </div>
+
+                    <div>
+                        <h3 class="text-lg font-semibold mb-2">Contact Us</h3>
+                        <p class="text-gray-400">Email: </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     
